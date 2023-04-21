edc242e98d587c6e1869a79c4dee4c05e982eb5a
Felix <9974641+jfelixh@users.noreply.github.com>
2023-04-21 17:02:43 +0200

    Finished Dev Setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      ignored: "**/node_modules",
+      poll: 1000,
+      aggregateTimeout: 500,
+    },
+  }),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
