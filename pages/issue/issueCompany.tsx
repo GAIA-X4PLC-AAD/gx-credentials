c08270ff170924094b34d24f72de76ae3fbd2551
Parshant Singh <parshant.e2@gmail.com>
2023-06-16 10:16:00 +0000

    Updated table UI

--- pages/issue/issueCompany.tsx
+++ pages/issue/issueCompany.tsx
@@ -89,86 +89,85 @@ export default function IssueCompany(props: any) {
     }
   };
 
+  const whiteShadow = {
+    boxShadow: "0px 0px 10px 3px rgba(255,255,255,0.75)",
+  };
+
   return (
     <main className="ml-20 mt-10">
       <h2>Pending Company Applications</h2>
       <div className="flex flex-col w-5/6">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <table className="min-w-full text-center text-sm font-light">
-                <thead className="border-b bg-neutral-50 font-medium border-neutral-500 text-neutral-800">
-                  <tr>
-                    <th scope="col" className=" px-6 py-4">
-                      GX ID
-                    </th>
-                    <th scope="col" className=" px-6 py-4">
-                      Company Name
-                    </th>
-                    <th scope="col" className=" px-6 py-4">
-                      Public Key Hash
-                    </th>
-                    <th scope="col" className=" px-6 py-4">
-                      Description
-                    </th>
-                    <th scope="col" className=" px-6 py-4"></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {applications.map((application: any) => (
-                    <tr
-                      className="border-b border-neutral-500"
-                      key={application.address}
-                    >
-                      <td className="whitespace-nowrap  px-6 py-4 font-medium">
-                        {application.gx_id}
-                      </td>
-                      <td className="whitespace-nowrap  px-6 py-4">
-                        {application.name}
-                      </td>
-                      <td className="whitespace-nowrap  px-6 py-4">
-                        {application.address}
-                      </td>
-                      <td className="whitespace-nowrap  px-6 py-4">
-                        {application.description}
-                      </td>
-                      {application.status === "pending" ? (
-                        <td className="whitespace-nowrap px-6 py-4">
-                          <button
-                            onClick={() =>
-                              handleAcceptCompanyIssuance(application)
-                            }
-                            className="mr-2"
-                          >
-                            Accept
-                          </button>
-                          <button
-                            onClick={() =>
-                              handleRejectCompanyIssuance(application)
-                            }
-                          >
-                            Reject
-                          </button>
-                        </td>
-                      ) : application.status === "approved" ? (
-                        <td>
-                          <span className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-green-200 text-green-800">
-                            Approved
-                          </span>
-                        </td>
-                      ) : (
-                        <td>
-                          <span className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium  bg-red-200 text-red-700">
-                            Rejected
-                          </span>
-                        </td>
-                      )}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+        <div
+          className="overflow-x-auto shadow rounded-lg border border-white"
+          style={whiteShadow}
+        >
+          <table className="min-w-full text-center text-sm font-light">
+            <thead className="border-b bg-gray-500 font-medium border-neutral-500 text-black">
+              <tr>
+                <th scope="col" className=" px-6 py-4">
+                  GX ID
+                </th>
+                <th scope="col" className=" px-6 py-4">
+                  Company Name
+                </th>
+                <th scope="col" className=" px-6 py-4">
+                  Public Key Hash
+                </th>
+                <th scope="col" className=" px-6 py-4">
+                  Description
+                </th>
+                <th scope="col" className=" px-6 py-4"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {applications.map((application: any) => (
+                <tr
+                  className="border-b border-neutral-500"
+                  key={application.address}
+                >
+                  <td className="whitespace-nowrap  px-6 py-4 font-medium">
+                    {application.gx_id}
+                  </td>
+                  <td className="whitespace-nowrap  px-6 py-4">
+                    {application.name}
+                  </td>
+                  <td className="whitespace-nowrap  px-6 py-4">
+                    {application.address}
+                  </td>
+                  <td className="whitespace-nowrap  px-6 py-4">
+                    {application.description}
+                  </td>
+                  {application.status === "pending" ? (
+                    <td className="whitespace-nowrap px-6 py-4">
+                      <button
+                        onClick={() => handleAcceptCompanyIssuance(application)}
+                        className="mr-2"
+                      >
+                        Accept
+                      </button>
+                      <button
+                        onClick={() => handleRejectCompanyIssuance(application)}
+                      >
+                        Reject
+                      </button>
+                    </td>
+                  ) : application.status === "approved" ? (
+                    <td>
+                      <span className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-green-200 text-green-800">
+                        Approved
+                      </span>
+                    </td>
+                  ) : (
+                    <td>
+                      <span className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium  bg-red-200 text-red-700">
+                        Rejected
+                      </span>
+                    </td>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </main>
