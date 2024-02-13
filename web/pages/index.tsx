c168c77494b0187ed68d40ec4d473e52f55fe80a
Felix Hoops <9974641+jfelixh@users.noreply.github.com>
2024-02-13 12:53:04 +0100

    Removed dark mode specific classes
    Signed-off-by: Felix Hoops <9974641+jfelixh@users.noreply.github.com>

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -104,7 +104,7 @@ export default function Home(props: any): JSX.Element {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p>This is experimental software. Use with caution!</p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://wwwmatthes.in.tum.de/pages/t5ma0jrv6q7k/sebis-Public-Website-Home"
