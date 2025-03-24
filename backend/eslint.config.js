e5e0b5161a7a0f5e2d52e6d178533baf51d29b6b
jfelixh <jfelixh@users.noreply.github.com>
2025-03-24 15:54:37 +0000

    Prettified Code!

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -3,10 +3,16 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
   tseslint.configs.recommended,
 ]);
