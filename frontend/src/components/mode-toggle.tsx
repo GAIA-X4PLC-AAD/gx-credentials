67532d7b3ce81ade092d2a7d670a7693bdaa800b
Felix Hoops <9974641+jfelixh@users.noreply.github.com>
2025-05-07 11:32:49 +0200

    Remove next-themes
    Signed-off-by: Felix Hoops <9974641+jfelixh@users.noreply.github.com>

--- frontend/src/components/mode-toggle.tsx
+++ frontend/src/components/mode-toggle.tsx
@@ -2,8 +2,8 @@
 
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
 
+import { useTheme } from "@/components/providers/theme-provider";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -19,8 +19,8 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <WalletProvider>
           <AppRouter />
           <Toaster />
